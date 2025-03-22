7ef5f078e4b1b21f7bda766badb64926948da6eb
李涛 <litao@shouqianba.com>
2025-03-22 12:27:29 +0800

    只保留工作台导航栏，并设置登录成功之后重定向到工作台

--- config/routes.ts
+++ config/routes.ts
@@ -12,12 +12,6 @@
  */
 export default [
   {
-    path: '/workbench',
-    name: 'workbench',
-    icon: 'desktop',  // 或其他你想要的图标
-    component: './Workbench',
-  },
-  {
     path: '/user',
     layout: false,
     routes: [
@@ -29,37 +23,14 @@ export default [
     ],
   },
   {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: '/admin',
-        redirect: '/admin/sub-page',
-      },
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        component: './Admin',
-      },
-    ],
-  },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
+    path: '/workbench',
+    name: 'workbench',
+    icon: 'desktop',
+    component: './Workbench',
   },
   {
     path: '/',
-    redirect: '/welcome',
+    redirect: '/user/login',  // 修改这里，重定向到登录页
   },
   {
     path: '*',
